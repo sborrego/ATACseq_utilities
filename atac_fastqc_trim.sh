f4eba00bf149f8cc35fb1ebeb9efca8841ee4ce5
Stacey Borrego <staceyborrego@gmail.com>
2018-10-31 19:26:15 -0700

    added script

---
+++ atac_fastqc_trim.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#$ -o /som/sborrego/201810_ATACSEQ_MB468_R8/fastqc/fastqc.out
+#$ -e /som/sborrego/201810_ATACSEQ_MB468_R8/fastqc/fastqc.err 
+#$ -q free64,som,asom
+#$ -pe openmp 8
+#$ -m beas
+#$ -ckpt blcr
+
+set -euxo pipefail
+
+module load blcr
+module load fastqc/0.11.7
+
+
+EXP_DIR=/som/sborrego/201810_ATACSEQ_MB468_R8
+TRIM_DATA=${EXP_DIR}/trim_data
+
+QC_OUT_DIR=/som/sborrego/201810_ATACSEQ_MB468_R8/fastqc_trim
+QC_HTML_DIR=${QC_OUT_DIR}/fastqc_trim_html
+
+mkdir -p ${QC_OUT_DIR}
+mkdir -p ${QC_HTML_DIR}
+
+for FILE in `find ${TRIM_DATA} -name \*.gz`; do
+    fastqc $FILE \
+    --threads 8 \
+    --outdir ${QC_OUT_DIR}
+
+    mv ${QC_OUT_DIR}/*.html ${QC_HTML_DIR}
+done
+
+tar -C ${QC_OUT_DIR} -czvf ${QC_HTML_DIR}.tar.gz ${QC_HTML_DIR} 

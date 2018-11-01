62e11cfe3d7886d72fe9885e5b3ce46aea2c54a2
Stacey Borrego <staceyborrego@gmail.com>
2018-11-01 14:01:43 -0700

    updated

--- atac_bowtie_align.sh
+++ atac_bowtie_align.sh
@@ -14,8 +14,8 @@ set -euxo pipefail
 module load bowtie2/2.2.7
 module load samtools/1.0
 
-GENOME_CHROMS=/som/sborrego/refs/hg38_chroms_only
-CHR_MITO=/som/sborrego/refs/hg38_Mitochondial_DNA
+GENOME_CHROMS=/som/sborrego/refs/hg38_chroms_only/hg38_chr_only
+CHR_MITO=/som/sborrego/refs/hg38_Mitochondial_DNA/chrM
 
 EXP_DIR=/som/sborrego/201810_ATACSEQ_MB468_R8
 DATA_DIR=${EXP_DIR}/"$1"
@@ -28,6 +28,9 @@ mkdir -p ${ALIGN_DIR}
 mkdir -p ${ALIGN_MITO_DIR}
 mkdir -p ${UNALIGN_MITO_DIR}
 
+RUNLOG=${ALIGN_DIR}/runlog_align_${1}.txt
+echo "Run by `whoami` on `date`" > ${RUNLOG}
+
 for NUMBER in `seq 1 2`; do
 	for FILE in `find ${DATA_DIR} -name \*P"${NUMBER}"\*1P.fq.gz`; do
 		PREFIX=`basename ${FILE} _1P.fq.gz`
@@ -37,16 +40,27 @@ for NUMBER in `seq 1 2`; do
 		UNALIGNED_MITO_FILE=${UNALIGN_MITO_DIR}/${PREFIX}_mito_unaligned.fq.gz
 		ALIGNED_MITO_FILE=${ALIGN_MITO_DIR}/${PREFIX}_mito_alignment.bam
 
-		echo ${FILE}
-		echo ${PREFIX}
-		echo ${R1}
-		echo ${R2}
-		echo ${UNALIGNED_MITO_FILE}
-		echo ${ALIGNED_MITO_FILE}
+		# echo ${FILE}
+		# echo ${PREFIX}
+		# echo ${R1}
+		# echo ${R2}
+		# echo ${UNALIGNED_MITO_FILE}
+		# echo ${ALIGNED_MITO_FILE}
+
+		echo "*** Aligning: ${PREFIX}"
+		echo "Alignment summary for ${PREFIX}" >> ${RUNLOG}
+
+		bowtie2 \
+		--threads 32 \
+		-x ${CHR_MITO} \
+		--un-conc-gz ${UNALIGNED_MITO_FILE} -1 ${R1} -2 ${R2} \
+		| samtools view -Sb - > ${ALIGNED_MITO_FILE} \
+		2>> ${RUNLOG}
+
 	done
 done
 
 
-# bowtie2 -x ${CHR_MITO} --un-conc-gz ${UNALIGN_MITO}/${PREFIX} -1 YOUR_READ1.fq -2 YOUR_READ2.fq | samtools view -Sb -> mito.bam
+# bowtie2 -x ${CHR_MITO} --un-conc-gz ${UNALIGNED_MITO_FILE} -1 ${R1} -2 ${R2} | samtools view -Sb -> mito.bam
 
 # bowtie2 --threads 32 -x GENOME_CHROMS -1 unaln_1.fq -2 unaln_2.fq | samtools view -Sb -> YOUR_sample.bam

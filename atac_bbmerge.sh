9bcda6aa602a41b618064acbcaab846c3cf3a4f7
Stacey Borrego <staceyborrego@gmail.com>
2018-12-04 13:39:43 -0800

    adding bbmerge script

---
+++ atac_bbmerge.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+    
+#$ -o /som/sborrego/201810_ATACSEQ_MB468_R8/qsub_reports/bbmerge.out
+#$ -e /som/sborrego/201810_ATACSEQ_MB468_R8/qsub_reports/bbmerge.err
+#$ -q free64,som,asom,pub64 
+#$ -pe make 16
+#$ -R y 
+#$ -t 1-12 
+#$ -m beas            
+#$ -ckpt blcr  
+
+set -euxo pipefail
+
+if [ $# -ne 1 ]; then
+    echo "usage: data_dir"
+    exit 1
+fi
+
+# Provide directory name for alignment ($1) on command line
+
+# Location of BBMerge
+bbmerge=/data/apps/anaconda/3.7-5.3.0/bin/bbmerge.sh 
+
+# Logs to keep track of things
+RUNLOG=${ALIGN_DIR}/runlog_chrOnly_align_${SGE_TASK_ID}.txt
+echo "Run by `whoami` on `date`" >> ${RUNLOG}
+
+FLAG=${ALIGN_DIR}/alignment_chrOnly_errors_${SGE_TASK_ID}.flagstat
+echo "Run by `whoami` on `date`" >> ${FLAG}
+
+# Working directories
+EXP_DIR=/som/sborrego/201810_ATACSEQ_MB468_R8
+DATA_DIR=${EXP_DIR}/"$1"
+RESULTS=${EXP_DIR}/bbmerge_results
+
+# Make non-existent files 
+mkdir -p ${RESULTS}
+
+# File for lists of files to be processed
+LIST_1=${DATA_DIR}/read1_list.txt
+LIST_2=${DATA_DIR}/read2_list.txt
+
+# Selecting file names using task ID
+READ_1=${DATA_DIR}/`head -n $SGE_TASK_ID $LIST_1 | tail -n 1`
+READ_2=${DATA_DIR}/`head -n $SGE_TASK_ID $LIST_2 | tail -n 1`
+
+$bbmerge \
+in1=$READ_1 in2=$READ_2 \
+outa=${RESULTS}/adapters_${SGE_TASK_ID}.fa

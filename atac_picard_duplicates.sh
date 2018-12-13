274240af983a1a28cf52c00ce021c3519d629ab0
Stacey Borrego <staceyborrego@gmail.com>
2018-12-13 10:33:49 -0800

    adding script

---
+++ atac_picard_duplicates.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+    
+#$ -o /som/sborrego/201810_ATACSEQ_MB468_R8/qsub_reports/markDuplicates.out
+#$ -e /som/sborrego/201810_ATACSEQ_MB468_R8/qsub_reports/markDuplicates.err
+#$ -q free64,som,asom,pub64 
+#$ -pe make 16
+#$ -R y 
+#$ -t 1-6 
+#$ -m beas            
+#$ -ckpt blcr  
+
+set -euxo pipefail
+
+module load picard-tools/1.96
+module load samtools/1.0
+
+MARK_DUPS=/data/apps/picard-tools/1.96/MarkDuplicates.jar
+
+BAM_DIR=/som/sborrego/201810_ATACSEQ_MB468_R8/alignments/data_chr_trim_clip_SORTED
+BAM_LIST=sorted_bam_list.txt
+NO_DUPS_DIR=/som/sborrego/201810_ATACSEQ_MB468_R8/alignments/data_final_181213
+
+mkdir -p ${NO_DUPS_DIR}
+
+BAM_INPUT=${BAM_DIR}/`head -n $SGE_TASK_ID $BAM_LIST | tail -n 1`
+BAM_OUTPUT=`head -n $SGE_TASK_ID $BAM_LIST | tail -n 1 | cut -d. -f1`
+
+java -Xmx2g -jar ${MARK_DUPS} \
+INPUT=${BAM_INPUT} \
+OUTPUT=${NO_DUPS_DIR}/${BAM_OUTPUT}.sorted.nodup.bam \
+METRICS_FILE=${NO_DUPS_DIR}/${BAM_OUTPUT}.marked_dup_metrics.txt \
+REMOVE_DUPLICATES=true

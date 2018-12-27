3cb2005f59cf9f92bfe868b0d39eddd30775243e
Stacey Borrego <staceyborrego@gmail.com>
2018-12-27 16:40:50 -0700

    added header removal

--- atac_sort_merge.sh
+++ atac_sort_merge.sh
@@ -42,20 +42,25 @@ RUNLOG=${RUNLOG_DIR}/runlog_sortNmerge_${SGE_TASK_ID}.txt
 echo "Run by `whoami` on `date`" >> ${RUNLOG}
 echo ${INPUT_PREFIX} >> ${RUNLOG}
 
-# Convert peak files to bed format
-pos2bed.pl ${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200.txt > ${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200.not_fixed.bed
-pos2bed.pl ${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500.txt > ${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500.not_fixed.bed
+# Removing header from peak files
+cat ${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200.txt | sed '/^\#/d' > ${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200_headless.txt
+cat ${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500.txt | sed '/^\#/d' > ${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500_headless.txt
 
+# Convert peak files to bed format
+pos2bed.pl ${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200_headless.txt > ${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200.not_fixed.bed 2>> ${RUNLOG}
+pos2bed.pl ${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500_headless.txt > ${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500.not_fixed.bed 2>> ${RUNLOG}
+
+# Fixing column numbers that may be reversed
 awk '{OFS="\t"} {if ($3<$2) print $1,$3,$2 ; else print $0}' ${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200.not_fixed.bed > ${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200.bed
 awk '{OFS="\t"} {if ($3<$2) print $1,$3,$2 ; else print $0}' ${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500.not_fixed.bed > ${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500.bed
 
 # Sort and merge bed files
 sort -k1,1 -k2,2n \
-${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200.txt \
-${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500.txt \
+${PEAK_200}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak200.bed \
+${PEAK_500}/${INPUT_PREFIX}/${INPUT_PREFIX}_peak500.bed \
 | mergeBed -c 4,5 -o sum,collapse -i - > ${PEAK_MERGE}/${INPUT_PREFIX}.merge.bed 2>> ${RUNLOG}
 
 # Merge single sorted file - results in final peak list
 bedtools merge -c 4,5 -o sum,collapse \
--i ${PEAK_MERGE}/${INPUT_PREFIX}.merge.bed > ${PEAK_FINAL}/${INPUT_PREFIX}.finalpeaks.txt 2>> ${RUNLOG}
+-i ${PEAK_MERGE}/${INPUT_PREFIX}.merge.bed > ${PEAK_FINAL}/${INPUT_PREFIX}.finalpeaks.bed 2>> ${RUNLOG}
 

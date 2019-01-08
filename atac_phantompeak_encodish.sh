99e478d45207afc9e3fcc9affc607d28c65b28b8
Stacey Borrego <staceyborrego@gmail.com>
2019-01-07 23:54:57 -0800

    adding script

---
+++ atac_phantompeak_encodish.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+    
+#$ -o /som/sborrego/201810_ATACSEQ_MB468_R8/qsub_reports/encode_xCorrelate.out
+#$ -e /som/sborrego/201810_ATACSEQ_MB468_R8/qsub_reports/encode_xCorrelate.err
+#$ -q free64,som,asom,pub64 
+#$ -pe make 16
+#$ -R y 
+#$ -t 1-6 
+#$ -m as            
+#$ -ckpt blcr
+
+set -euxo pipefail
+
+module load R/3.5
+module load boost/1.63.0
+module load samtools
+
+# Loading phantompeakqualtools program
+SPP=/som/sborrego/programs/phantompeakqualtools/run_spp.R
+
+# Input files
+TAG_ALIGN_DIR=/som/sborrego/201810_ATACSEQ_MB468_R8/analysis_encode/alignments/tagAlign
+TAG_LIST=${TAG_ALIGN_DIR}/tag_list.txt
+
+# Taskarray file name
+TAG_INPUT=`head -n $SGE_TASK_ID $TAG_LIST | tail -n 1`
+TAG_OUTPUT=`head -n $SGE_TASK_ID $TAG_LIST | tail -n 1 | cut -d. -f1 | cut -d/ -f 8 | cut -d_ -f 1`
+
+# Output directory
+PHANTOM_OUTPUT=/som/sborrego/201810_ATACSEQ_MB468_R8/analysis_encode/alignments/peak
+
+# Using variable names atacseq pipeline
+CC_SCORES_FILE="${PHANTOM_OUTPUT}/${TAG_OUTPUT}.cc.qc"
+CC_PLOT_FILE="${PHANTOM_OUTPUT}/${TAG_OUTPUT}.cc.plot.pdf"
+
+# CC_SCORE FILE format
+# Filename <tab> numReads <tab> estFragLen <tab> corr_estFragLen <tab> PhantomPeak <tab> corr_phantomPeak <tab> argmin_corr <tab> min_corr <tab> phantomPeakCoef <tab> relPhantomPeakCoef <tab> QualityTag
+
+Rscript ${SPP} \
+-c=${SUBSAMPLED_TA_FILE} \
+-p=8 \
+-filtchr=chrM \
+-savp=${CC_PLOT_FILE} \
+-out=${CC_SCORES_FILE}
+
+sed -r 's/,[^\t]+//g' ${CC_SCORES_FILE} > temp
+mv temp ${CC_SCORES_FILE}

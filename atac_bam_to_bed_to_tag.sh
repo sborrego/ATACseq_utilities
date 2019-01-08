04563235f1ee497426f89177bd27e1f72db132ff
Stacey Borrego <staceyborrego@gmail.com>
2019-01-07 17:11:21 -0800

    debugging

--- atac_bam_to_bed_to_tag.sh
+++ atac_bam_to_bed_to_tag.sh
@@ -41,10 +41,10 @@ echo ${BAM_INPUT} >> ${RUNLOG}
 echo ${BAM_OUTPUT} >> ${RUNLOG}
 
 # Create tagAlign File
-bedtools bamtobed \
--i ${BAM_INPUT} \
-| awk 'BEGIN{OFS="\t"}{$4="N"; $5="1000"; print $0}' \
-| gzip -c > ${FINAL_TAG_ALIGN_FILE}.PE2SE.tagAlign.gz 2>> ${RUNLOG}
+# bedtools bamtobed \
+# -i ${BAM_INPUT} \
+# | awk 'BEGIN{OFS="\t"}{$4="N"; $5="1000"; print $0}' \
+# | gzip -c > ${FINAL_TAG_ALIGN_FILE}.PE2SE.tagAlign.gz 2>> ${RUNLOG}
 
 # Name sorting deduped files for BEDPE file
 java -Xmx2g -jar ${SORTSAM} \
